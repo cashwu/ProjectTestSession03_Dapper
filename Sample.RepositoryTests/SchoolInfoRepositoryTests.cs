7a678c5e367c7784395a1dd138744bc59b8c7fb2
cash <cash@cashwu.com>
2017-12-09 11:50:37 +0800

    repo test

--- Sample.RepositoryTests/SchoolInfoRepositoryTests.cs
+++ Sample.RepositoryTests/SchoolInfoRepositoryTests.cs
@@ -1,20 +1,173 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Dapper;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 using Sample.Repository;
-using System;
-using System.Collections.Generic;
+using Sample.Repository.Database;
+using Sample.RepositoryTests.Misc;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Sample.Repository.Tests
+namespace Sample.RepositoryTests
 {
     [TestClass()]
+    [DeploymentItem(@"TestData\SampleDB_SchoolInfo_Create.sql")]
+    [DeploymentItem(@"TestData\SampleDB_SchoolInfo_Data.sql")]
     public class SchoolInfoRepositoryTests
     {
+        private IDatabaseConnectionFactory DatabaseConnectionFactory { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.DatabaseConnectionFactory = Substitute.For<IDatabaseConnectionFactory>();
+
+            this.DatabaseConnectionFactory
+                .Create()
+                .Returns(new SqlConnection(TestHook.SampleDbConnection));
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize()]
+        public static void TestClassInitialize(TestContext testContext)
+        {
+            CreateTable();
+            PrepareData();
+        }
+
+        private static void CreateTable()
+        {
+            using (var conn = new SqlConnection(TestHook.SampleDbConnection))
+            {
+                conn.Open();
+
+                using (SqlTransaction trans = conn.BeginTransaction())
+                {
+                    var script = File.ReadAllText(@"SampleDB_SchoolInfo_Create.sql");
+                    conn.Execute(sql: script, transaction: trans);
+                    trans.Commit();
+                }
+            }
+        }
+
+        private static void PrepareData()
+        {
+            using (var conn = new SqlConnection(TestHook.SampleDbConnection))
+            {
+                conn.Open();
+
+                using (SqlTransaction trans = conn.BeginTransaction())
+                {
+                    var script = File.ReadAllText(@"SampleDB_SchoolInfo_Data.sql");
+                    conn.Execute(sql: script, transaction: trans);
+                    trans.Commit();
+                }
+            }
+        }
+
+        [ClassCleanup()]
+        public static void TestClassCleanup()
+        {
+            using (var conn = new SqlConnection(TestHook.SampleDbConnection))
+            {
+                conn.Open();
+                string sqlCommand = TableCommands.DropTable("SchoolInfo");
+                conn.Execute(sqlCommand);
+            }
+        }
+
+        private SchoolInfoRepository GetSystemUnderTest()
+        {
+            var sut = new SchoolInfoRepository(this.DatabaseConnectionFactory);
+            return sut;
+        }
+
         [TestMethod()]
         public void GetAllSchoolInfosTest()
         {
-            Assert.Fail();
+            // arrange
+            int expected = 4035;
+
+            var sut = this.GetSystemUnderTest();
+
+            // act
+            var actual = sut.GetAllSchoolInfos();
+
+            // assert
+            actual.Should().NotBeNull();
+            actual.Any().Should().BeTrue();
+            actual.Count.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void GetByCity_CityId輸入0000_應取得台北市所有學校資料()
+        {
+            // arrange
+            string cityId = "0000";
+
+            var sut = this.GetSystemUnderTest();
+
+            // act
+            var actual = sut.GetByCity(cityId);
+
+            // assert
+            actual.Any().Should().BeTrue();
+            actual.All(x => x.CityId == cityId).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void GetByDistrict_CityId輸入0001_DistrictId輸入247_應取得新北市蘆洲區的所有學校資料()
+        {
+            // arrange
+            string cityId = "0001";
+            int districtId = 247;
+
+            var sut = this.GetSystemUnderTest();
+
+            // act
+            var actual = sut.GetByDistrict(cityId, districtId);
+
+            // assert
+            actual.Any().Should().BeTrue();
+            actual.All(x => x.CityId == cityId && x.DistrictId == districtId).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void GetByConditions_CityId輸入0008_DistrictId輸入320_Caegory為5_應取得桃園市中壢區的所有大學資料()
+        {
+            // arrange
+            string cityId = "0008";
+            int districtId = 320;
+            int category = 5;
+
+            var sut = this.GetSystemUnderTest();
+
+            // act
+            var actual = sut.GetByConditions(cityId, districtId, category);
+
+            // assert
+            actual.Any().Should().BeTrue();
+            actual.All(x => x.CityId == cityId && x.DistrictId == districtId && x.Category == category).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Get_Id輸入104526_應取得縣立阿里山國中小的學校資料()
+        {
+            // arrange
+            string id = "104526";
+
+            string expected = "縣立阿里山國中(小)";
+
+            var sut = this.GetSystemUnderTest();
+
+            // act
+            var actual = sut.Get(id);
+
+            // assert
+            actual.Should().NotBeNull();
+            actual.Name.Should().Be(expected);
         }
     }
 }
---
+++ Sample.RepositoryTests/TestHook.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample.RepositoryTests.Misc;
+
+namespace Sample.RepositoryTests
+{
+    [TestClass]
+    public class TestHook
+    {
+        internal static string SampleDbConnection => 
+            string.Format(TestDbConnection.LocalDb.LocalDbConnectionString, DatabaseName.SampleDB);
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            // Create SampleDB
+            var sampleDbDatabase = new TestDbUtilities(DatabaseName.SampleDB);
+            if (sampleDbDatabase.IsLocalDbExists())
+            {
+                sampleDbDatabase.DeleteLocalDb();
+            }
+            sampleDbDatabase.CreateDatabase();
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            // Delete SampleDB
+            var defaultDatabase = new TestDbUtilities(DatabaseName.Default);
+            defaultDatabase.DeleteLocalDb(SampleDbConnection);
+        }
+    }
+}
