3ff82613845f266ab4ddb6575ebe0c3dcf701b51
cash <cash@cashwu.com>
2017-12-09 11:39:09 +0800

    Add project files.

---
+++ Sample.RepositoryTests/Misc/TestDbConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample.RepositoryTests.Misc
+{
+    public class TestDbConnection
+    {
+        public class LocalDb
+        {
+            public const string LocalDbConnectionString =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog={0};Integrated Security=True;MultipleActiveResultSets=True";
+
+            public static string Default =>
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True";
+        }
+    }
+}
